be2d05d985972f713e81396f04e74457c844a94d
vanhhn <vietanhvmo@gmail.com>
2024-03-19 21:31:44 +0700

    ok

--- codeOnLuyenCode/A.cpp
+++ codeOnLuyenCode/A.cpp
@@ -1,42 +1,44 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
-
-string checkRepresentation(int x) {
-    // Tạo một mảng động để lưu trữ các giá trị
-    vector<bool> canRepresent(x + 1, false);
-    
-    // Số 1 luôn có thể biểu diễn được
-    canRepresent[1] = true;
-
-    // Duyệt qua từng số từ 2 đến x
-    for (int i = 2; i <= x; i++) {
-        // Kiểm tra xem số i có thể biểu diễn được không
-        // Bằng cách thử tất cả các số 1 có thể được thêm vào tổng
-        for (int j = 1; j < i; j++) {
-            if (canRepresent[j] && i % j == 0) {
-                canRepresent[i] = true;
-                break;
+#define ll long long
+#define mod 1000000007
+int a[1000];
+void snt(){
+    memset(a,1,sizeof(a));
+    a[0]=a[1]=0;
+    for(int i=2;i*i<1000;i++){
+        if(a[i]){
+            for(int j=i*i;j<1000;j+=i){
+                a[j]=0;
             }
         }
     }
-    
-    // Kiểm tra xem x có thể biểu diễn được hay không
-    if (canRepresent[x]) {
-        return "YES";
-    }
-    return "NO";
 }
-
-int main() {
-    int N;
-    cin >> N;
-    
-    // Kiểm tra xem N có thể biểu diễn được hay không
-    string result = checkRepresentation(N);
-    
-    // In kết quả
-    cout << result << endl;
-    
+int check(int n){
+    int sum=0;
+    while(n!=0){
+        int so=n%10;
+        sum+=so*so;
+        n/=10;
+    }
+    return a[sum];
+}
+int main()
+{
+    snt();
+    int t;
+    cin >> t;
+    while (t--)
+    { 
+        int n;
+        cin>>n;
+       for(int i=n+1;;i++){
+        if(check(i)){
+            cout<<i<<"\n";
+            break;
+        }
+       }
+        
+    }
     return 0;
 }
